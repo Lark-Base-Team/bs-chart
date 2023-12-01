4ef8e61e56acc23dae96e6d760dbe9fa2814980e
WumaCoder <15804854160@163.com>
2023-12-01 14:00:35 +0800

    chore: 发布

---
+++ release/_next/static/0zsmYE4OSRm0ry_sY0z3-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-f25df21f3e633978.js"],"/_error":["static/chunks/pages/_error-fc83f3cd32085222.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
